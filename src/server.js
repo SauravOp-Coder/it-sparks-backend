3205b65ba004b53d57e63e8770e79ec1525b69a7
SauravOp-Coder <sauravshinde992@gmail.com>
2026-06-26 19:35:31 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 connectCloudinary();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://itsparks.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
