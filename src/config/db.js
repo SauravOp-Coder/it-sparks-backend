fce63e73e257c98145c9799d5704c16ee044e564
SauravOp-Coder <sauravshinde992@gmail.com>
2026-08-06 17:13:12 +0530

    c

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("Mongo URI:", process.env.MONGO_URI);
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`MongoDB Connection Error: ${error.message}`);
+    console.error("Full MongoDB Error:");
+    console.error(error);
     process.exit(1);
   }
 };
